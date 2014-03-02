b8d187857a4835a6ca7ea9aa1cb576bea033966b
Corey Gelbaugh <ncgelbaugh11@aol.com>
2014-03-02 16:45:02 -0500

    icons and snippets for iphone app

---
+++ CD-iPhone/Snippets/BlueLibrary/LibraryAPI.h
@@ -0,0 +1,15 @@
+//
+//  LibraryAPI.h
+//  BlueLibrary
+//
+//  Created by Eli Ganem on 15/8/13.
+//  Copyright (c) 2013 Eli Ganem. All rights reserved.
+//
+
+#import "Album.h"
+
+@interface LibraryAPI : NSObject
+
++ (LibraryAPI*)sharedInstance;
+
+@end
---
+++ CD-iPhone/Snippets/BlueLibrary/PersistencyManager.m
@@ -0,0 +1,23 @@
+//
+//  PersistencyManager.m
+//  BlueLibrary
+//
+//  Created by Eli Ganem on 15/8/13.
+//  Copyright (c) 2013 Eli Ganem. All rights reserved.
+//
+
+#import "PersistencyManager.h"
+
+@implementation PersistencyManager
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+        }
+    return self;
+}
+
+@end
